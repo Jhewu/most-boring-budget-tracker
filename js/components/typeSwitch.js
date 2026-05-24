0dda4345e3404d9baf55f1eb029fea652f3bda93
jhewu <junyi.hewu2003@gmail.com>
2026-05-23 23:08:40 -0400

    add non-selectable default value

--- js/components/typeSwitch.js
+++ js/components/typeSwitch.js
@@ -1,6 +1,4 @@
-```
-Handles category switch if type is 'income' or 'expense'
-```
+// Handles category switch if type is 'income' or 'expense'
 
 // Fetch the 'type' and 'categories' Selection
 const typeSelect = document.querySelector('#type'); 
@@ -8,9 +6,9 @@ const categorySelect = document.querySelector('#categories');
 
 // Define categories options
 const categoryOptions = {
-    income: ['Active', 'Passive', 'Miscellaneous'],
-    expense: ['Bill', 'Utils', 'Health', 'Shopping', 'Entertainment', 'Food', 'Groceries', 'Transportation', 'Miscellaneous']
-}
+    income: ['Select a Category', 'Active', 'Passive', 'Miscellaneous'],
+    expense: ['Select a Category' , 'Bill', 'Utils', 'Health', 'Shopping', 'Entertainment', 'Food', 'Groceries', 'Transportation', 'Miscellaneous']
+}                
 
 typeSelect.addEventListener('change', function() {
     const selectedType = this.value; 
@@ -20,12 +18,19 @@ typeSelect.addEventListener('change', function() {
     categorySelect.innerHTML = ''; 
 
     // Add new options
+    let first = true; 
     for (let cat of categories) {
         const option = document.createElement('option'); 
+        if (first) {
+            option.disabled = true; 
+            first = false;
+        }
+        
         option.value = cat; 
         option.textContent = cat; 
         categorySelect.appendChild(option); 
     }
+    categorySelect.value = 'Select a Category';
 })
 
 // Trigger change event on page load to populate initial categories
